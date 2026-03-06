f0213ed15ca54c93bebce1de004a40ccbd8fced0
Pratham159 <prathamkansara524@gmail.com>
2026-03-06 20:00:19 +0530

    Add files via upload

---
+++ IRIS.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix 
+
+X,y = load_iris(return_X_y=True)
+print("Shape of X:", X.shape)
+print("Shape of Y:", y.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=50)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.fit_transform(X_test)
+
+model = LogisticRegression()
+model.fit(X_train,y_train)
+
+y_pred = model.predict(X_test)
+
+print("Confusion Matrix")
+print(confusion_matrix(y_test,y_pred))
